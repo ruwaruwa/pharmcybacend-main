cd4112557dcb5c7ff815e7a96e683b3c17c02854
ruwaruwa <almozali287@gmail.com>
2023-11-27 06:22:30 -0800

    apdated

--- DB_Config/Connection.js
+++ DB_Config/Connection.js
@@ -1,7 +1,10 @@
 const moongoose=require('mongoose');
 const ConnectDB= async()=>{
     try {
-        moongoose.connect("mongodb+srv://amiira123:12345@cluster0.xdvvzfc.mongodb.net/Pharmacy_system")
+
+      //  moongoose.connect("mongodb+srv://amiira123:12345@cluster0.xdvvzfc.mongodb.net/Pharmacy_system")
+moongoose.connect('mongodb+srv://amiira123:12345@cluster0.xdvvzfc.mongodb.net/pharmcy_backend');
+
         console.log('DB Connected !!');
 
     } catch (error) {
